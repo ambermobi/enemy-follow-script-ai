a628b1f54a85a09ea19b74c7731be00cf91c739d
Bharat <bharatappguru@gmail.com>
2015-02-25 12:27:59 +0530

    Enemy Follow Script (AI) Tutorial

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CharacterMovement : MonoBehaviour {
+	
+	public float moveSpeed=10.0F;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//Get Input From User
+		Vector3 input = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+
+		//Apply Force as per requirement
+		rigidbody.AddForce (input*moveSpeed);
+
+
+}
+	void Update()
+	{
+		//To Give a Jump Effect
+		if (Input.GetKey (KeyCode.Space)) {
+			rigidbody.AddForce(Vector3.up,ForceMode.Impulse);		
+		}
+	}
+
+}
---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowScript : MonoBehaviour {
+	public Transform Character;
+	public float speed=0.1F;
+	private Vector3 directionOfCharacter;
+	private bool challenged=false; // Check if touched
+
+	void Update () {
+		if (challenged) {
+						// Get direction to move towards
+						directionOfCharacter = Character.transform.position - transform.position;
+						directionOfCharacter = directionOfCharacter.normalized;
+						
+						transform.Translate (directionOfCharacter * speed, Space.World);
+				} 
+	}
+
+	// trigged on player hit
+	void OnTriggerEnter( Collider other)
+	{
+		if (other.CompareTag ("Player")) {
+			challenged=true;
+		}
+	}
+}
+
